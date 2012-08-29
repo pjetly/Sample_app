01ea787245ab575e7fe8394d89f50b07f7d5f595
Prateek Jetly <pjetly@hotmail.com>
2012-08-28 23:48:36 -0700

    Finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home Page" do
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Home')
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "Help Page" do
+    it "should have the content help page" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  describe "About Page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+  describe "Contacts Page" do
+    it "should have the H1 'Contacts'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+
+end
