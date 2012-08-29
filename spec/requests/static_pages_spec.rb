2620f48eb2c9ea7828a0c1cb1f96c53a3ea7612e
Prateek Jetly <pjetly@hotmail.com>
2012-08-29 15:12:28 -0700

    title helpers

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let base_title = 'Ruby on Rails Tutorial Sample App'
+
   describe "Home Page" do
     it "should have the content 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Home')
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -14,7 +16,7 @@ describe "Static Pages" do
     it "should have the content help page" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -22,7 +24,7 @@ describe "Static Pages" do
     it "should have the content 'About'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About')
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+      page.should have_selector('title', :text => "#{base_title} | About")
     end
   end
 
