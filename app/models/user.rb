88a92bbffb7fb754eb13df900dce2b61285aeca1
Prateek Jetly <pjetly@hotmail.com>
2012-09-27 13:45:00 -0700

    Added user signin page

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  
-
   before_save { |user| user.email = user.email.downcase }
 
   validates :name, presence: true, length:  { maximum: 50 }
@@ -24,7 +22,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             uniqueness: {case_sensitive: false }
 
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, length: {minimum: 6}
   validates :password_confirmation, presence: true
 
 
