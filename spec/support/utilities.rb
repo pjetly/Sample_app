73e6948583671ff239d67db636acd6fec547840a
Prateek Jetly <pjetly@hotmail.com>
2012-10-02 17:43:07 -0700

    Add cucumber etc

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,11 @@
+include ApplicationHelper
+
+RSpec::Matchers.define :have_error_message do | message |
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: 'Invalid')
+  end
+end
+
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
@@ -6,4 +14,5 @@
       "#{base_title} | #{page_title}"
     end
   end
+
 
