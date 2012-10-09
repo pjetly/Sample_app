677b61d32ee98d3dc50fde16538fbf06d6051ec9
Prateek Jetly <pjetly@hotmail.com>
2012-10-09 14:30:31 -0700

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,12 +6,8 @@ module SessionsHelper
     Rails.logger.debug "setting current user = #{user.name} or #{self.current_user.name}"
   end
 
-  def signed_in?  
-    if !current_user.nil?
-      Rails.logger.debug "current_user = #{current_user.name}"
-    else
-      Rails.logger.debug "***** current_user is empty *****"
-    end
+  def signed_in?
+    Rails.logger.debug "Current_user.nil? = #{current_user.nil?}"
     !current_user.nil?
   end
 
@@ -20,11 +16,25 @@ module SessionsHelper
   end
 
   def current_user
+    Rails.logger.debug "********Current user = #{@current_user} ********"
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+  
+  def current_user?(user)
+    user == current_user
   end
 
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
   end
 end
